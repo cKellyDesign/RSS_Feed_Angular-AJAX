9e82022907fe6591400721163698343bed717f21
Conor Kelly <ckelly@ckellydesign.net>
2014-07-24 13:01:30 +0300

    Implements Grunt Sass and Watch Tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,54 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     
+    sass: {
+      dist: {
+        files: {
+          'style.css' : 'style.scss'
+        },
+        dev: {
+          options: {
+            sourceMap: true
+          },
+          files: {
+            'style.css' : 'style.scss'
+          }
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: '*.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      },
+      scripts: {
+        files: '*.js',
+        options: {
+          livereload: true
+        }
+      }
+    },
+
     connect: {
       server: {
         options: {
-          keepalive: true
+          keepalive: false,
+          livereload: true
+            
+          
         }
       }
     }
+
   })
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'connect', 'watch']);
 };
